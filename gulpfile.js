d819941deec1f37d902f59ae32a7a4ee3e3d4c91
calvinvette <calvin@dragontale.com>
2015-04-17 09:49:25 -0400

    20150417@09.50

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ var uglify = require('gulp-uglifyjs');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
 var livereload = require('gulp-livereload');
+var karma = require('karma').server;
 
 var src = {
 	css : [ 'src/**/*.css' ],
@@ -50,7 +51,25 @@ gulp.task('concat', function() {
 	.pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('watch', function() {
-	livereload.listen();
-	gulp.watch(src.all, [ 'uglify' ]);
+//gulp.task('watch', function() {
+//	livereload.listen();
+//	gulp.watch(src.all, [ 'uglify' ]);
+//});
+
+gulp.task('test', function(done) {
+	gulp.src(['!node_modules','!bower_components/**/node_modules/','bower_components/**/*.js'])
+		.pipe(concat('vendor-scripts.js'))
+		.pipe(gulp.dest('development'));
+	gulp.src(['!node_modules/**','!**/*.test.js','!bower_components/**','**/*.js'])
+		.pipe(concat('app-scripts.js'))
+		.pipe(gulp.dest('development'));
+	console.log('karma.start: ' + karma.start);
+	return karma.start({ configFile : __dirname + '/karma.conf.js'}, done);
 });
+
+gulp.task('watch', function() {
+	gulp.watch(['**/*.test.js'], ['test']);
+});
+
+
+
